4f2e9a1ca2e924a156168fd409d6f4643905babf
Akhshy Ganesh <52190273+akhshyganesh@users.noreply.github.com>
2024-05-26 19:24:32 +0530

    Adding JS Files (#1)
    * Adding JS Files
    
    * Add Something Function
    
    * Add setTimeout Func
    
    * Add SetInterval Func

---
+++ index.js
@@ -0,0 +1,13 @@
+// empty file
+
+function doSomething () {
+    console.log('Did Something');
+}
+
+setTimeout(() => {
+    console.log('After 1 sec');
+}, 1000);
+
+setInterval(() => {
+    console.log('Every 1 sec');
+}, 1000);
